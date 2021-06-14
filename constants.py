b48519de7f5180a5f64f2d301decb122f873519a
kevslinger <siouxman@me.com>
2021-06-14 15:05:14 -0400

    change google_chrome to google-chrome

--- constants.py
+++ constants.py
@@ -3,5 +3,5 @@ ADMISSIONS_TAB_NAME = "kevTesting"
 HYPERLINK = "HYPERLINK"
 REDDIT_URL = "https://www.reddit.com/"
 REDDITMETIS_URL = "https://redditmetis.com/user/"
-GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google-chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
--- fill_sheet.py
+++ fill_sheet.py
@@ -22,14 +22,15 @@ def main():
     # Get the specific spreadsheet tab
     admissions_tab = sheet.worksheet(constants.ADMISSIONS_TAB_NAME)
     # Set the options for our chrome driver
+    os.system("which google-chrome")
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.binary_location = constants.GOOGLE_CHROME_PATH
     # Get the Chrome Driver
-    #driver = webdriver.Chrome(executable_path=constants.CHROMEDRIVER_PATH,
-    #                          options=chrome_options)
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(executable_path=constants.CHROMEDRIVER_PATH,
+                              options=chrome_options)
+    #driver = webdriver.Chrome(options=chrome_options)
     # Call the function to look through each of the new rows on the sheet
     start_col = "G"
     end_col = "W"
