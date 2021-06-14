d7997cfc7efbbcd922845294f52a6f783b0ba78b
kevslinger <siouxman@me.com>
2021-06-14 13:27:44 -0400

    update constants to reflect buildpack locations of chrome and driver

--- constants.py
+++ constants.py
@@ -3,5 +3,5 @@ ADMISSIONS_TAB_NAME = "kevTesting"
 HYPERLINK = "HYPERLINK"
 REDDIT_URL = "https://www.reddit.com/"
 REDDITMETIS_URL = "https://redditmetis.com/user/"
-GOOGLE_CHROME_PATH = '/bin/google-chrome'
-CHROMEDRIVER_PATH = './chromedriver'
+GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
